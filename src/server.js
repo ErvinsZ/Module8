cc160bfd547cd9afc943e89692430364d3e0d2b4
ErvinsZ <63841322+ErvinsZ@users.noreply.github.com>
2020-08-25 13:32:52 +0300

    Post working

---
+++ src/server.js
@@ -0,0 +1,42 @@
+const express = require("express")
+const { join } = require("path")
+const listEndpoints = require("express-list-endpoints")
+const mongoose = require("mongoose")
+const usersRouter = require("./users")
+
+const {
+  notFoundHandler,
+  forbiddenHandler,
+  badRequestHandler,
+  genericErrorHandler,
+} = require("./errorHandlers")
+
+const server = express()
+
+
+const port = process.env.PORT || 3003
+
+const staticFolderPath = join(__dirname, "../public")
+server.use(express.static(staticFolderPath))
+server.use(express.json())
+server.use("/users", usersRouter)
+
+server.use(badRequestHandler)
+server.use(forbiddenHandler)
+server.use(notFoundHandler)
+server.use(genericErrorHandler)
+
+console.log(listEndpoints(server))
+
+mongoose
+  .connect("mongodb://localhost:27017/module8-day1", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+  })
+  .then(
+    server.listen(port, () => {
+      console.log("Running on port", port)
+    })
+  )
+  .catch((err) => console.log(err))
